5588423fc1f6ac751973a504d5de107c19eb1e77
karllaloane <karllaloane@gmail.com>
2023-06-12 19:10:19 -0300

    Adição de documentação do swagger

--- src/main/java/com/construcao/financiase/user/controller/UserController.java
+++ src/main/java/com/construcao/financiase/user/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
-public class UserController{
+public class UserController implements UserControllerDocs{
 
     private final UserService userService;
 
