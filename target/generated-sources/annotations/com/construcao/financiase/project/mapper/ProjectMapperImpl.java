e62cd6eb0a21810459b2691234452ea42ac75274
karllaloane <karllaloane@gmail.com>
2023-06-16 21:32:54 -0300

    configuração ambiente prod e dev

--- target/generated-sources/annotations/com/construcao/financiase/project/mapper/ProjectMapperImpl.java
+++ target/generated-sources/annotations/com/construcao/financiase/project/mapper/ProjectMapperImpl.java
@@ -7,7 +7,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-06-16T20:34:46-0300",
+    date = "2023-06-16T21:31:54-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 20.0.1 (Oracle Corporation)"
 )
 public class ProjectMapperImpl implements ProjectMapper {
